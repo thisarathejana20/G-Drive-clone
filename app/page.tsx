f5edf465e52641265f79bc1d5d69e44b3a3b637f
thisaramax20 <thisarathejana20@gmail.com>
2025-02-22 06:51:43 +0530

    create reusable tailwind classes as a separate set

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="flex-center h-screen">
       <h1 className="text-3xl">StoreIt - The only storage solution you need</h1>
     </div>
   );
