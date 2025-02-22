e1fce31ddcf9927c36fe69757775e8f8d03e0dc1
thisaramax20 <thisarathejana20@gmail.com>
2025-02-22 07:55:31 +0530

    create icons & images repository

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React from "react";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen">
+      <section className="bg-rose-300 p-10">
+        <div>
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto"
+          />
+          <div className="space-y-5 text-white">
+            <h1 className="text-[34px] leading-[42px] font-bold">
+              Manage your files the best way
+            </h1>
+            <p className="text-[16px] leading-[24px] font-normal-1">
+              This is a place where you can store all your documents
+            </p>
+          </div>
+          <Image
+            src="/assets/images/files.png"
+            alt="hero"
+            width={342}
+            height={342}
+            className="transition-all hover:rotate-2 hover:scale-105"
+          />
+        </div>
+      </section>
+      {children}
+    </div>
+  );
+};
+
+export default layout;
