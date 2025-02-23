fa11cda1c763a7f0df9eb0cdd587272fa806c2d3
thisaramax20 <thisarathejana20@gmail.com>
2025-02-23 07:20:56 +0530

    create handle new account server action

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,79 @@
+"use server";
+
+import { ID, Query } from "node-appwrite";
+import { createAdminClient } from "../appwrite";
+import { appWriteConfig } from "../appwrite/config";
+import { error } from "console";
+import { parseStringify } from "../utils";
+
+//server action
+//// ******* Create account flow
+// 1.User enter fullname and email
+// 2.Check above
+// 3.Send OTP
+// 4.Send a secret key to generate a session
+// 5.Create a new user document if not exists
+// 6.Return userId to complete login
+// 7.Login user
+
+const handleError = (error: unknown, message: string) => {
+  console.error(error);
+  throw new Error(message);
+};
+
+const getUserByEmail = async (email: string) => {
+  const { database } = await createAdminClient();
+
+  const result = await database.listDocuments(
+    appWriteConfig.databaseId,
+    appWriteConfig.userCollectionId,
+    [Query.equal("email", email)]
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send OTP");
+  }
+};
+
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP({ email });
+
+  if (!accountId) throw new Error("Failed to send an OTP");
+
+  if (!existingUser) {
+    const { database } = await createAdminClient();
+
+    await database.createDocument(
+      appWriteConfig.databaseId,
+      appWriteConfig.userCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:
+          "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
+        accountId,
+      }
+    );
+  }
+
+  return parseStringify(accountId);
+};
