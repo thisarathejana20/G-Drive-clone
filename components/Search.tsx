e232cf24e9036ab514e1bda84d4557390851b3a8
thisaramax20 <thisarathejana20@gmail.com>
2025-03-02 21:40:19 +0530

    add search UI

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,7 +1,100 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { Input } from "./ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Models } from "node-appwrite";
+import { getFiles } from "@/lib/actions/file.actions";
+import Thumbnails from "./Thumbnails";
+import FormattedDateTime from "./FormattedDateTime";
 
 const Search = () => {
-  return <div>Search</div>;
+  const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const path = usePathname();
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      if (!query) {
+        setResults([]);
+        setOpen(false);
+        return router.push(path.replace(searchParams.toString(), ""));
+      }
+      const files = await getFiles({ searchText: query });
+
+      setResults(files.documents);
+      setOpen(true);
+    };
+    fetchFiles();
+  }, [query]);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery("");
+    }
+  }, [searchQuery]);
+
+  const handleClickItem = (file: Models.Document) => {
+    setOpen(false);
+    setResults([]);
+
+    router.push(
+      `/${file.type === "video" ? "media" : file.type + "s"}?query=${query}`
+    );
+  };
+
+  return (
+    <div className="search">
+      <div className="search-input-wrapper">
+        <Image
+          src="/assets/icons/search.svg"
+          alt="search"
+          width={24}
+          height={24}
+        />
+        <Input
+          value={query}
+          placeholder="Search..."
+          onChange={(e) => setQuery(e.target.value)}
+          className="search-input"
+        />
+        {open && (
+          <ul className="search-result">
+            {results.length > 0 ? (
+              results.map((file) => (
+                <li
+                  key={file.id}
+                  className="flex items-center justify-between"
+                  onClick={handleClickItem}
+                >
+                  <div className="flex cursor-pointer items-center gap-4">
+                    <Thumbnails
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className="size-9 min-w-9"
+                    />
+                    <p className="subtitle-2 line-clamp-1">{file.name}</p>
+                  </div>
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className="caption line-clamp-1"
+                  />
+                </li>
+              ))
+            ) : (
+              <p>No results found</p>
+            )}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
